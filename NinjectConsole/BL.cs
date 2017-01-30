922701baac802613d30021eda0a1672f6d3bf01c
sansalk-Sanjaya Dodamgoda <dodangoda@gmail.com>
2017-01-30 10:12:44 +0000

    Add project files.

---
+++ NinjectConsole/BL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NinjectConsole
+{
+    class BL
+    {
+        IProduct objIpro;
+
+        public BL(IProduct objIproduct)
+        {
+            objIpro = objIproduct;
+        }
+
+        public void Insert()
+        {
+            objIpro.InsertProduct();
+        }
+
+    }
+}
---
+++ NinjectConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ninject;
+using System.Reflection;
+
+namespace NinjectConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initializes a new instance of the Ninject.StandardKernel class.
+            StandardKernel _Kernal = new StandardKernel();
+
+            //load Modules
+            _Kernal.Load(Assembly.GetExecutingAssembly());
+
+            //Gets an instance of the specified service. 
+            IProduct _objIpro = _Kernal.Get<IProduct>();
+
+            // In this step Ninject will inject DL object to BL 
+            BL objbl = new BL(_objIpro);
+
+            // calling method of BL from that Dependency 
+            objbl.Insert(); 
+
+            Console.ReadLine();
+        }
+    }
+}
